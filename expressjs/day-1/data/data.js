70b010538f1a6410964d6544dfd7a9493890fa01
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-01 18:32:38 +0530

    http methods

---
+++ expressjs/day-1/data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    name: "milan_koradiya",
+    displayname: "Milan Koradiya"
+  },
+  {
+    id: 2,
+    name: "rahul_sharma",
+    displayname: "Rahul Sharma"
+  },
+  {
+    id: 3,
+    name: "priya_patel",
+    displayname: "Priya Patel"
+  },
+  {
+    id: 4,
+    name: "ankit_verma",
+    displayname: "Ankit Verma"
+  },
+  {
+    id: 5,
+    name: "neha_mehta",
+    displayname: "Neha Mehta"
+  }
+]
